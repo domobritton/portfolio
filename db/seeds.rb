2e3be4eb5ba6be1683aad6efefeb6ee5efbf47ce
domobritton <domonicbritton@gmail.com>
2018-07-02 15:00:26 -0700

    Implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
 
+puts "3 topics created"
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Lorem ipsum dolor amet palo santo brooklyn next level four loko sartorial celiac hammock pop-up irony health goth pickled humblebrag. Mustache cray heirloom forage asymmetrical mixtape pabst organic vinyl la croix keffiyeh. Direct trade bushwick poke pork belly marfa typewriter yuccie. Tumblr butcher schlitz bitters, vape kale chips PBR&B. Organic bicycle rights pop-up shoreditch gluten-free listicle pork belly everyday carry."
+    body: "Lorem ipsum dolor amet palo santo brooklyn next level four loko sartorial celiac hammock pop-up irony health goth pickled humblebrag. Mustache cray heirloom forage asymmetrical mixtape pabst organic vinyl la croix keffiyeh. Direct trade bushwick poke pork belly marfa typewriter yuccie. Tumblr butcher schlitz bitters, vape kale chips PBR&B. Organic bicycle rights pop-up shoreditch gluten-free listicle pork belly everyday carry.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -24,10 +31,19 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "My great service" ,
+    subtitle: "Ruby on Rails" ,
+    body: "Lorem ipsum dolor amet palo santo brooklyn next level four loko sartorial celiac hammock pop-up irony health goth pickled humblebrag. Mustache cray heirloom forage asymmetrical mixtape pabst organic vinyl la croix keffiyeh.",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Angular" ,
     body: "Lorem ipsum dolor amet palo santo brooklyn next level four loko sartorial celiac hammock pop-up irony health goth pickled humblebrag. Mustache cray heirloom forage asymmetrical mixtape pabst organic vinyl la croix keffiyeh.",
     main_image: "http://via.placeholder.com/600x400",
     thumb_image: "http://via.placeholder.com/350x200"
